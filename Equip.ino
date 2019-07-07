1e7ae7fcfc62c4593b505240b17461e4aee37057
Jorge Lopez <jorge.lopez@riseup.net>
2019-07-07 17:45:05 +0200

    Substituido char* con la ca por la incorporacion de un archivo en la memoria SPIFFS

--- Equip.ino
+++ Equip.ino
@@ -3,6 +3,7 @@
 #include "IBeaconProvider_ESP32.h"
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <SPIFFS.h>
 
 #define MQTT_SERVER "10.42.0.1"
 #define MQTT_PORT 8883
@@ -25,29 +26,6 @@ unsigned long lastTimeActivate;
 
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
-
-const char* caCert =
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIDcjCCAlqgAwIBAgIJANKAZDMg41XXMA0GCSqGSIb3DQEBCwUAME4xCzAJBgNV\n" \
-"BAYTAkVTMQswCQYDVQQIDAJFUzELMAkGA1UEBwwCRVMxCzAJBgNVBAoMAkVTMQsw\n" \
-"CQYDVQQLDAJjYTELMAkGA1UEAwwCY2EwHhcNMTkwNzA3MTQ1MTAwWhcNMTkwODA2\n" \
-"MTQ1MTAwWjBOMQswCQYDVQQGEwJFUzELMAkGA1UECAwCRVMxCzAJBgNVBAcMAkVT\n" \
-"MQswCQYDVQQKDAJFUzELMAkGA1UECwwCY2ExCzAJBgNVBAMMAmNhMIIBIjANBgkq\n" \
-"hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0S/uzVO2O4hB6/Dp6sF+qXa8F4T7nHE1\n" \
-"T2ojeLRpipLvu8psbLVdDFHDgTImefPPpPmd0gUh0kPxBFbgAdiG7LvLiRWmVhaT\n" \
-"Igoms6XAAnEBaW3XwF92IsO8M9vq6IAQgsx2zb23uVyFX1CY1bRXNY+OGvE7CtgH\n" \
-"ksJXgmm0p+4JPBWholMupBdbS81Sy5bShjsoD/KEz0eKgl0XQH2nxQYlSYzjno6b\n" \
-"zfTl+qvP1ntIFdeCTxW/+3c0AQDL6wj3Pi/z2Pt+sK+rsR5t+2T8SgrQIZHJ2tO4\n" \
-"bQdIAaHjqxonDBQCA7B/6GytyWmZ2GJ4WjVh+cksLsVkjtW7bfwuEQIDAQABo1Mw\n" \
-"UTAdBgNVHQ4EFgQUAJYd84p/PH8bIZpbku12NyRxcRIwHwYDVR0jBBgwFoAUAJYd\n" \
-"84p/PH8bIZpbku12NyRxcRIwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsF\n" \
-"AAOCAQEALvgNYcpEH5WmeYGYiyXqqmtW9ss+YVu/bIB8tJZnw2yh7ORKev1857bQ\n" \
-"rCC53TkmNPLthAEoWeAfHd971SzP+wPGThbzZfUGY+DfvHVqq5ZGvOG1ZSVUQTqh\n" \
-"PAZJ9V1CZgJTkcJvb+tICT41K82MiaYfcI9qUwgJ6ewLEQqYx0nonhnDxOOp698H\n" \
-"DYW5r8SPTHJ3sCrNsWKQnBua768F73qq29UhWEggt5dw07q/A2suCxAuahqvQDr1\n" \
-"UVFQf4vi0wWx7odtl7SRbk5bQD42LZHFjRlWcF7Xnk09fSrIe9VRf/dSEEUdesse\n" \
-"gKzpCYwLc8J5yo9efKVX6byXX+s/7w==\n" \
-"-----END CERTIFICATE-----";
 
 void setup() {
   Serial.begin(115200);
@@ -84,7 +62,14 @@ void setupWifi(){
   }
   printf("\n");
 
-  espClient.setCACert(caCert);
+  SPIFFS.begin(true);
+  File ca = SPIFFS.open("/ca.crt", "r");
+  if(!ca) {
+    printf("Fallo ao abrir o certificado!\n");
+    return;
+  }
+
+  espClient.loadCACert(ca, ca.size());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
